96fd9d2ebffd167217609d485005560eb90fd0b5
Sebastien Chapuis <sebastien@chapu.is>
2022-02-10 18:50:08 +0100

    Change garbage collector algo

--- tezos/context/src/gc/worker.rs
+++ tezos/context/src/gc/worker.rs
@@ -11,11 +11,15 @@ use std::{
 
 use crossbeam_channel::{Receiver, RecvError};
 
-use crate::{chunks::ChunkedVec, kv_store::HashId, serialize::in_memory::iter_hash_ids};
+use crate::{
+    chunks::ChunkedVec,
+    kv_store::{index_map::IndexMap, HashId},
+    serialize::in_memory::iter_hash_ids,
+};
 
 use tezos_spsc::Producer;
 
-use super::sorted_map::SortedMap;
+// use super::sorted_map::SortedMap;
 
 pub(crate) const PRESERVE_CYCLE_COUNT: usize = 2;
 
@@ -30,6 +34,10 @@ pub(crate) struct GCThread {
     pub(crate) recv: Receiver<Command>,
     pub(crate) pending: Vec<HashId>,
     pub(crate) debug: bool,
+
+    pub(crate) global: IndexMap<HashId, Option<Arc<[u8]>>>,
+    pub(crate) global_counter: IndexMap<HashId, Option<u8>>,
+    pub(crate) counter: u8,
 }
 
 pub(crate) enum Command {
@@ -38,7 +46,10 @@ pub(crate) enum Command {
         new_ids: ChunkedVec<HashId>,
     },
     MarkReused {
+        values_in_block: ChunkedVec<(HashId, Arc<[u8]>)>,
+        new_ids: ChunkedVec<HashId>,
         reused: ChunkedVec<HashId>,
+        commit_hash_id: HashId,
     },
     Close,
 }
@@ -108,8 +119,15 @@ impl GCThread {
                 Ok(Command::StartNewCycle {
                     values_in_cycle,
                     new_ids,
-                }) => self.start_new_cycle(values_in_cycle, new_ids),
-                Ok(Command::MarkReused { reused }) => self.mark_reused(reused),
+                }) => {
+                    // self.start_new_cycle(values_in_cycle, new_ids)
+                }
+                Ok(Command::MarkReused {
+                    reused,
+                    values_in_block,
+                    new_ids,
+                    commit_hash_id,
+                }) => self.mark_reused(reused, values_in_block, new_ids, commit_hash_id),
                 Ok(Command::Close) => {
                     elog!("GC received Command::Close");
                     break;
@@ -137,7 +155,7 @@ impl GCThread {
                 values_in_cycle.len(),
                 new_ids.len()
             ),
-            Ok(Command::MarkReused { reused }) => format!("REUSED {:?}", reused.len()),
+            Ok(Command::MarkReused { reused, .. }) => format!("REUSED {:?}", reused.len()),
             Ok(Command::Close { .. }) => "CLOSE".to_owned(),
             Err(_) => "ERR".to_owned(),
         };
@@ -151,10 +169,10 @@ impl GCThread {
 
         let mut total = 0;
 
-        for (index, c) in self.cycles.list.iter().enumerate() {
-            log!("CYCLE[{:?}]_LENGTH={:?}", index, c.len());
-            total += c.len();
-        }
+        // for (index, c) in self.cycles.list.iter().enumerate() {
+        //     log!("CYCLE[{:?}]_LENGTH={:?}", index, c.len());
+        //     total += c.len();
+        // }
         log!(
             "PENDING={:?} TOTAL_IN_CYCLES={:?}",
             self.pending.len(),
@@ -169,26 +187,26 @@ impl GCThread {
     ) {
         GC_PENDING_HASHIDS.store(self.pending.len(), Ordering::Release);
 
-        // Gather `HashId` created before a commit.
-        // We send them back to the main thread, they can be reused
-        let mut hashid_without_value = Vec::with_capacity(1024);
+        // // Gather `HashId` created before a commit.
+        // // We send them back to the main thread, they can be reused
+        // let mut hashid_without_value = Vec::with_capacity(1024);
 
-        let new_cycle = new_cycle.into_hash_map();
+        // let new_cycle = new_cycle.into_hash_map();
 
-        for hash_id in new_ids.iter() {
-            if !new_cycle.contains_key(hash_id) {
-                hashid_without_value.push(*hash_id);
-            }
-        }
+        // for hash_id in new_ids.iter() {
+        //     if !new_cycle.contains_key(hash_id) {
+        //         hashid_without_value.push(*hash_id);
+        //     }
+        // }
 
-        self.send_unused(hashid_without_value);
+        // self.send_unused(hashid_without_value);
 
-        if self.debug {
-            log!("GC_WORKER: START_NEW_CYCLE NEW_CYCLE={:?}", new_cycle.len(),);
-        }
+        // if self.debug {
+        //     log!("GC_WORKER: START_NEW_CYCLE NEW_CYCLE={:?}", new_cycle.len(),);
+        // }
 
-        let unused = self.cycles.roll(new_cycle);
-        self.send_unused(unused);
+        // let unused = self.cycles.roll(new_cycle);
+        // self.send_unused(unused);
     }
 
     /// Notify the main thread that the ids are free to reused
@@ -238,61 +256,159 @@ impl GCThread {
         GC_PENDING_HASHIDS.store(self.pending.len(), Ordering::Release);
     }
 
-    fn mark_reused(&mut self, mut reused: ChunkedVec<HashId>) {
-        let now = std::time::Instant::now();
+    fn traverse_mark(&mut self, hash_id: HashId, counter: u8, traversed: &mut usize) {
+        *traversed += 1;
 
-        GC_PENDING_HASHIDS.store(self.pending.len(), Ordering::Release);
+        let value = {
+            let value = self.global.get(hash_id).unwrap().unwrap().as_ref().unwrap();
+            Arc::clone(value)
+        };
 
-        let param = reused.len();
-        let mut none = 0;
-        let mut moved = 0;
-        let mut last_cycle = 0;
-        let mut total = 0;
+        {
+            let value_counter = self
+                .global_counter
+                .get_mut(hash_id)
+                .unwrap()
+                .unwrap()
+                .as_mut()
+                .unwrap();
+            *value_counter = counter;
+        }
 
-        while let Some(hash_id) = reused.pop() {
-            total += 1;
+        for hash_id in iter_hash_ids(&value) {
+            self.traverse_mark(hash_id, counter, traversed);
+        }
+    }
 
-            let value = match self.cycles.move_to_last_cycle(hash_id) {
-                Some(v) => {
-                    moved += 1;
-                    v
-                }
-                None => {
-                    let last = self.cycles.list.back().unwrap();
-                    match last.get(&hash_id).cloned() {
-                        Some(v) => {
-                            last_cycle += 1;
-                            v
-                        }
-                        None => {
-                            none += 1;
-                            continue;
-                        }
-                    }
-                }
+    fn take_unused(&mut self) -> Vec<HashId> {
+        let current_counter = self.counter;
+
+        let mut unused = Vec::with_capacity(2048);
+
+        for (hash_id, value) in self.global_counter.iter_with_keys() {
+            let value = match value {
+                Some(value) => value,
+                None => continue,
             };
 
-            for hash_id in iter_hash_ids(&value) {
-                reused.push(hash_id);
+            if current_counter < 5 {
+                continue;
+            }
+
+            if *value < current_counter - 3 {
+                self.global.insert_at(hash_id, None).unwrap();
+                unused.push(hash_id);
             }
         }
 
-        if self.debug {
-            log!(
-                "MARK_REUSED PARAM={:?} TOTAL={:?} MOVED={:?} LAST_CYCLE={:?} NONE_VALUES={:?} {:?}",
-                param,
-                total,
-                moved,
-                last_cycle,
-                none,
-                now.elapsed(),
-            );
+        unused
+    }
+
+    fn mark_reused(
+        &mut self,
+        mut reused: ChunkedVec<HashId>,
+        mut values_in_blocks: ChunkedVec<(HashId, Arc<[u8]>)>,
+        new_ids: ChunkedVec<HashId>,
+        commit_hash_id: HashId,
+    ) {
+        let now = std::time::Instant::now();
+
+        while let Some(chunk) = values_in_blocks.pop_first_chunk() {
+            for (hash_id, value) in chunk.into_iter() {
+                self.global.insert_at(hash_id, Some(value)).unwrap();
+                self.global_counter
+                    .insert_at(hash_id, Some(self.counter))
+                    .unwrap();
+            }
         }
 
-        self.send_pending();
+        // Gather `HashId` created before a commit.
+        // We send them back to the main thread, they can be reused
+        let mut hashid_without_value = Vec::with_capacity(1024);
 
-        for store in self.cycles.list.iter_mut() {
-            store.shrink_to_fit();
+        for hash_id in new_ids.iter() {
+            if self
+                .global
+                .get(*hash_id)
+                .map(|v| v.is_none())
+                .unwrap_or(true)
+            {
+                hashid_without_value.push(*hash_id);
+            }
         }
+
+        let mut traversed = 0;
+        self.traverse_mark(commit_hash_id, self.counter, &mut traversed);
+
+        let unused = self.take_unused();
+
+        let mut sent = hashid_without_value.len();
+        sent += unused.len();
+
+        self.send_unused(hashid_without_value);
+        self.send_unused(unused);
+
+        log!(
+            "MARK_REUSED SENT={:?} TRAVERSED={:?} TIME={:?}",
+            sent,
+            traversed,
+            now.elapsed(),
+        );
+
+        self.counter = self.counter.wrapping_add(1);
+
+        // GC_PENDING_HASHIDS.store(self.pending.len(), Ordering::Release);
+
+        // let param = reused.len();
+        // let mut none = 0;
+        // let mut moved = 0;
+        // let mut last_cycle = 0;
+        // let mut total = 0;
+
+        // while let Some(hash_id) = reused.pop() {
+        //     total += 1;
+
+        //     let value = match self.cycles.move_to_last_cycle(hash_id) {
+        //         Some(v) => {
+        //             moved += 1;
+        //             v
+        //         }
+        //         None => {
+        //             let last = self.cycles.list.back().unwrap();
+        //             match last.get(&hash_id).cloned() {
+        //                 Some(v) => {
+        //                     last_cycle += 1;
+        //                     v
+        //                 }
+        //                 None => {
+        //                     none += 1;
+        //                     continue;
+        //                 }
+        //             }
+        //         }
+        //     };
+
+        //     for hash_id in iter_hash_ids(&value) {
+        //         reused.push(hash_id);
+        //     }
+        // }
+
+        // if self.debug {
+        //     log!(
+        //         "MARK_REUSED PARAM={:?} TOTAL={:?} MOVED={:?} LAST_CYCLE={:?} NONE_VALUES={:?} {:?}",
+        //         param,
+        //         total,
+        //         moved,
+        //         last_cycle,
+        //         none,
+        //         now.elapsed(),
+        //     );
+        // }
+
+        // self.send_pending();
+
+        // for store in self.cycles.list.iter_mut() {
+        //     store.shrink_to_fit();
+        // }
     }
 }
